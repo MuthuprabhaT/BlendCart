8bc67f7d46128f93b0002e070d548e5b7522d69e
MuthuprabhaT <prabhathangam507@hotmail.com>
2024-11-17 17:20:57 +0530

    backend deploy url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,7 @@
 export const BASE_URL =
-  process.env.NODE_ENV === "development" ? "http://localhost:8000" : "";
+  process.env.NODE_ENV === "development"
+    ? "https://blendcart.onrender.com"
+    : "";
 export const PRODUCTS_URL = "/api/products";
 export const USERS_URL = "/api/users";
 export const ORDERS_URL = "/api/orders";
